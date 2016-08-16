e4a3f8c55d5434dbf1515248653096f4dd191e1a
Mykyta Troianov <nicktroyanov@gmail.com>
2016-08-16 11:06:02 +0300

    Save json report to file

--- lib/rubycritic/generators/json/simple.rb
+++ lib/rubycritic/generators/json/simple.rb
@@ -1,13 +1,17 @@
 require 'json'
 require 'rubycritic/version'
+require 'pathname'
 
 module RubyCritic
   module Generator
     module Json
       class Simple
+
         def initialize(analysed_modules)
           @analysed_modules = analysed_modules
         end
+
+        FILE_NAME = 'report.json'
 
         def render
           JSON.dump(data)
@@ -23,6 +27,14 @@ module RubyCritic
             analysed_modules: @analysed_modules,
             score: @analysed_modules.score
           }
+        end
+
+        def file_directory
+          @file_directory ||= Pathname.new(Config.root)
+        end
+
+        def file_pathname
+          File.join(file_directory, FILE_NAME)
         end
       end
     end
